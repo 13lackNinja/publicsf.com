3eaac4c2873d2899bf32317f8c93da722b0efb44
13lackninja <jonathan.crawford55@gmail.com>
2019-08-23 16:13:28 -0700

    featured image formatting

--- client/src/FeaturedEvent.js
+++ client/src/FeaturedEvent.js
@@ -5,15 +5,26 @@ import './styles/FeaturedEvent.css'
 const FeaturedEvent = (props) => {
   return (
     <div className="featured-event">
-      <a href={props.url} target="_blank" rel="noopener noreferrer" className="featured-event-link-wrapper">
+      <a
+        href={props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="featured-event-link-wrapper"
+      >
         <img
+          className="featured-event-image"
           src={props.image}
-          alt="event_image"
+          alt={props.image}
         />
         <h3>{props.date}</h3>
         <h2>{props.name}</h2>
       </a>
-      <a href={props.url} className="pw-orange-button featured-event-button">Tickets</a>
+      <a
+        href={props.url}
+        className="pw-orange-button featured-event-button"
+      >
+        Tickets
+      </a>
     </div>
   )
 }
