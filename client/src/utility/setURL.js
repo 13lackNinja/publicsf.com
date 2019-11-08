5e9e5ded644394f677cc7efa88ed9b72a1c2c428
13lackninja <jonathan.crawford55@gmail.com>
2019-11-08 12:08:11 -0800

    changed link to use slug instead of event id

--- client/src/utility/setURL.js
+++ client/src/utility/setURL.js
@@ -1,10 +1,9 @@
 // Returns either an external or BigNeon URL for a given event.
 export default function setURL(event) {
-  const baseURL = "https://bigneon.com/events/"
   const trackingParams = "?utm_source=publicworks&utm_medium=venuewebsite";
 
   return event.is_external ?
     event.external_url
     :
-    baseURL + event.id + trackingParams;
+    event.url + trackingParams;
 }
