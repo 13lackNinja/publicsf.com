66b057c9ed6a0a6510b46bfec6788310c61042ff
13lackninja <jonathan.crawford55@gmail.com>
2019-12-03 12:23:51 -0800

    changed to mailchimp newlsetter signup

--- client/src/NewsletterSignUp.js
+++ client/src/NewsletterSignUp.js
@@ -1,58 +1,28 @@
 // NewsletterSignUp
-// Adds form submision to ticketfly email list.
+// Adds htmlForm submision to ticketfly email list.
 
 import React from 'react'
 
 import './styles/NewsletterSignUp.css'
 
-class NewsletterSignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      input: 'enter email address',
-      isSuccessful: false
-    }
-  }
-
-  handleChange(e) {
-    this.setState({ input: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-
-  }
-
-  render() {
-    return (
-      <div id="newsletter-sign-up">
-        <h1>Join the ranks</h1>
-        <h3>Members are eligible for free tickets, prizes, and more!</h3>
-        <form id='newsletter-signup-form'>
-          <input
-            type="text"
-            name="email"
-            placeholder={this.state.input}
-            onChange={this.handleChange}
-          />
-          {this.state.isSuccessful &&
-            <p id="newsletter-sign-up-success-message">Email received!</p>
-          }
-          <button
-            id="newsletter-sign-up-submit-button"
-            className="pw-action-button"
-            onClick={this.handleSubmit}
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    )
-  }
-}
-
+const NewsletterSignUp = () => (
+  <div id="newsletter-sign-up">
+    <div id="mc_embed_signup">
+      <form action="https://publicsf.us3.list-manage.com/subscribe/post?u=3b0bc3dbf838dd9d8ef88a9b3&amp;id=4361bb0396" method="post" id="mc-embedded-subscribe-htmlForm" name="mc-embedded-subscribe-htmlForm" className="validate" target="_blank" noValidate>
+        <div id="mc_embed_signup_scroll">
+	        <label htmlFor="mce-EMAIL">Join The Ranks</label>
+          <h2>Stay in the loop with our weekly newsletter</h2>
+	        <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
+          <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">
+            <input type="text" name="b_3b0bc3dbf838dd9d8ef88a9b3_4361bb0396" tabIndex="-1"/>
+          </div>
+          <div className="clear">
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+);
 
 export default NewsletterSignUp
