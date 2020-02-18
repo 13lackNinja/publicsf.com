dadb5f0735bd13b4b59ab8a7689d8a31fc086b04
13lackninja <jonathan.crawford55@gmail.com>
2020-02-18 14:53:52 -0800

    exact path match to staff dashboard

--- client/src/MainContent.js
+++ client/src/MainContent.js
@@ -9,7 +9,7 @@ import './styles/MainContent.css'
 const MainContent = () => (
   <div id="main-content">
     <Switch>
-      <Route path="/" component={Staff}></Route>
+      <Route exact path="/" component={Staff}></Route>
       <Route path="/menu" component={MenuDisplay}></Route>
       <Route component={NotFound}></Route>
     </Switch>
